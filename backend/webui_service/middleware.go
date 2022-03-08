2c0b5dc1d55ed92c04332775a2adc47151f221bc
Nathalie <nathalie21005@gmail.com>
2022-03-08 18:04:48 -0500

    test wb

--- backend/webui_service/middleware.go
+++ backend/webui_service/middleware.go
@@ -2,12 +2,14 @@ package webui_service
 
 import (
 	"github.com/gin-gonic/gin"
+  	"strings"
+	"github.com/free5gc/path_util"
 )
 
 var PublicPath string
 
 func init() {
-	PublicPath = "public"
+	PublicPath = path_util.Free5gcPath("free5gc/webconsole/public")
 }
 
 func ReturnPublic() gin.HandlerFunc {
@@ -15,8 +17,13 @@ func ReturnPublic() gin.HandlerFunc {
 		method := context.Request.Method
 		if method == "GET" {
 			destPath := PublicPath + context.Request.RequestURI
+			if strings.Contains(destPath, "etc") {
+				destPath = PublicPath
+			}else
 			if destPath[len(destPath)-1] == '/' {
 				destPath = destPath[:len(destPath)-1]
+			} else {
+				context.File(destPath)
 			}
 			context.File(destPath)
 		} else {
